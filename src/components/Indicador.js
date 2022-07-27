0f3a599124bd805a11d606a4a4bfc3267496d680
marianopettinati <marianopettinati@gmail.com>
2022-07-26 23:26:00 -0300

    avance en useEffect

--- src/components/Indicador.js
+++ src/components/Indicador.js
@@ -1,46 +1,26 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import "./Indicador.css"
+import React, { useEffect, useState } from "react";
+import "./Indicador.css";
 
-class Indicador extends Component {
+function Indicador () {
+    const [productos, setProductos] = useState ([]);
 
-    constructor (props) {
-        super(props);
-        this.state = {
-            total: 0,
-        }
-    }
+    useEffect (() => {
+        console.log("soy el FETCH")
+        fetch("http://localhost:3000/product/api/products")
+            .then (response => response.json())
+            .then (data => {
+                console.log(data)
+                setProductos (data.results)
+            })
+            .catch(err => console.log(err))
+    }, [] )
 
-    componentDidMount(){
-        console.log("Soy el didMount")
-        fetch ("http://localhost:3000/product/api/products")
-            .then ( response => response.json())
-            .then (data => {this.setState({cuenta : data.data.count})})
-            .catch (error => console.log(error))
-    }
-
-    componentDidUpdate(){
-        console.log("Soy el didUpdate")
-    }
-
-    render() {
-        console.log ("Soy el render")
-
-        let total;
-        if (this.state.total = 0) {
-            total = <p> 0 </p>
-        } else {
-            total = <p> { this.state.cuenta }</p>
-        }
-
-        return (
-            <div className="indicador-wraper">
-                <h1> PRODUCTOS </h1>                
-                { total }
-            </div>
-            
-        )
-    }
+    return (
+        <div className="indicador-wraper">
+            <h1> PRODUCTOS </h1>                
+            { productos }
+        </div>        
+    )
 };
 
 export default Indicador
